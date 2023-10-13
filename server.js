68f49e4441bc19b687b233311ff1c65fc4dca375
Chris Wallace <chriswallace7@protonmail.com>
2023-10-13 12:38:41 -0500

    updated

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const PORT = process.env.PORT || 4001;  // Set port dynamically for Heroku
+
 io.on('connection', (socket) => {
     console.log('New client connected');
 
@@ -20,4 +22,4 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(4001, () => console.log('Listening on port 4001'));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));  // Updated port assignment
