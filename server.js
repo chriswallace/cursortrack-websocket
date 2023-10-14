b30a5a31d5f85e503a2a8bdbbcce6e49a3fc9815
Chris Wallace <chriswallace7@protonmail.com>
2023-10-13 21:25:56 -0500

    update origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "*",  // Allow all origins
+        origin: "https://chriswallace.net",  // Allow one origin
         methods: ["GET", "POST"]
     }
 });
