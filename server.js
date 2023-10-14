e6111afa1e47df5383958a400e2ad3a8822340da
Chris Wallace <chriswallace7@protonmail.com>
2023-10-13 20:17:20 -0500

    add page changing

--- server.js
+++ server.js
@@ -15,12 +15,18 @@ const io = socketIo(server, {
 
 const PORT = process.env.PORT || 4001;  // Set port dynamically for Heroku
 let userEmojis = {};  // Object to store user emojis by socket id
+let userPages = {};  // Object to store user pages by socket id
 
 app.use(cors());  // Enable CORS for all routes
 
 io.on('connection', (socket) => {
     console.log('New client connected');
     userEmojis[socket.id] = "😀";  // Set default emoji for new connections
+
+    socket.on('pageChange', (data) => {
+        const { page } = data;
+        userPages[socket.id] = page;  // Update page
+    });
 
     socket.on('emojiUpdate', (data) => {
         const { emoji } = data;
@@ -30,12 +36,15 @@ io.on('connection', (socket) => {
 
     socket.on('cursorMove', (data) => {
         const { x, y, emoji } = data;
-        socket.broadcast.emit('cursorMove', { userId: socket.id, x, y, emoji });
+        const page = userPages[socket.id];  // Get page of sender
+        // Broadcast to all other users on the same page
+        socket.broadcast.to(page).emit('cursorMove', { userId: socket.id, x, y, emoji });
     });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
         delete userEmojis[socket.id];  // Remove emoji association on disconnect
+        delete userPages[socket.id];  // Remove page association on disconnect
         io.emit('cursorLeave', { id: socket.id });
     });
 });
